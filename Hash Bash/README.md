9a434fbb60ae13ac1f7680fd5b8dd671f33f9b32
Jake Beckman <Jbeckman@cedarville.edu>
2021-09-03 10:29:55 -0500

    Adding Features

--- Hash Bash/README.md
+++ Hash Bash/README.md
@@ -7,14 +7,14 @@ the opportunity to present a more feature-rich user experience at the cost of re
 If the compared values don't match, HashBash will display to the user the mismatches indices and their values.
 
 Possible features to add:
-    - Using len() to compare length before comparing indices.
+    (DONE) Using len() to compare length before comparing indices.
         # This will stop HashBash from returning all indices after an added or subtracted character as mismatched.
-    - Functionality to open and read hash values stored in files rather in addition to entered text.
+    (DONE) Functionality to open and read hash values stored in files rather in addition to entered text.
         # Explore whether this should be done as a popup window/filepath entry field that autofills the value
         # or as a native ability of the original entry field (checkbox or conditional logic-driven).
-    - Ability to save and output the passed comparisons as a file (JSON?)
+    (DONE) Ability to save and output the passed comparisons as a file (JSON?)
         # Even though HashBash only displays the title of the passed comparisons, adding the ability to save the
         # values associated with those titles in a dictionary would be easy.
-    - Create a "HELP" button.
+    (DONE) Create a "HELP" button.
         # This would open a popup dialog window explaining the features of the program to save space in the window.
     - Consider refactoring the code into multiple files for conciseness and readability.  
--- Hash Bash/hashbashList.py
+++ Hash Bash/hashbashList.py
@@ -2,7 +2,12 @@
 
 #GUI script to compare two values, usually sha256 or md5 hashes
 
-import tkinter as tk 
+import tkinter as tk
+from tkinter.filedialog import askopenfile as aof
+from tkinter.filedialog import asksaveasfile as asf
+import json
+
+## This section contains the main logic for the program ##
 
 #function to define the comparison action on enter and button press
 def compare(event=None):
@@ -23,6 +28,8 @@ def compare(event=None):
         lbl_result["text"] = str("Looks like a match!")
         _list_update(hashTitle)
         _list_display()
+
+        dictSecret[hashTitle] = [firstHash, secondHash, "passed"]
         
     else: # This loops through each entry and shows where they differ
         _find_mismatches(firstHash, secondHash)
@@ -53,7 +60,57 @@ def _list_display():
     ent_second.delete(0, "end")
     ent_title.delete(0, "end")
 
+## This section contains the new utilities to make the program more user-friendly ##
+
+# Open file explorer to open a file of the specified type
+def enter_from_file():
+    f = aof(mode='r',
+            filetypes=[('CSV', '*.csv'), 
+            ('JSON', '*.json'),
+            ('Text', "*.txt"),
+            ('All File Types', '*.*')])
+    if f is not None:
+        content = f.readlines()
+        ent_first.insert(index=1, string=content[0].strip())
+        ent_second.insert(index=1, string=content[1])
+
+# Open save dialog and export the contents of dictSecret to save dir
+def save_to_file():
+    filetypes = [("JSON", "*.json")]
+    saved = asf(filetypes=filetypes,
+                defaultextension=filetypes)
+    
+    saved.write(_prepare_json())
+
+# format the contents of dictSecret as JSON pretty-printed data
+def _prepare_json():
+    return json.dumps(dictSecret,
+                    indent=4,
+                    separators=(", ", ": "),
+                    sort_keys=True)
+
+# Create a popup window to display info on button press
+def help_me():
+    t = tk.Toplevel(window)
+    t.geometry("500x200")
+
+    lbl_help = tk.Label(master=t, text=
+    "This program is useful for comparing two values.\n"
+    "Some preliminary comparisons are made on the two entries before\n"
+    "they are iterated to verify that they exist and are the same length.\n\n"
+    "To enter values from a file, separate the two values on separate lines\n"
+    "with only two values in the file. The 'enter from a file' button will input them.\n\n"
+    "All passed comparisons will be saved behind the scenes regardless of falling\n"
+    "off the display list. Click the export button and pick a location/filename\n"
+    "to save the passed values in a .json file. As yet no other filetypes are available.\n\n"
+    "There will be more features coming, and help_me will be updated accordingly."
+    )
+    lbl_help.pack()
+
+## This section begins the GUI creation and layout ##
+
 listMain = ["", "", "", ""]
+dictSecret = {}
 
 #creating and labeling the window, also making it fixed size
 window = tk.Tk()
@@ -110,13 +167,37 @@ btn_compare = tk.Button(
 window.bind('<Return>', compare)
 lbl_result = tk.Label(master=window, text="No Result Yet")
 
+# Create a help button and call the help_me() function
+btn_help = tk.Button(
+    master=window,
+    text="Help",
+    command=help_me
+)
+
+# Create a button to call the enter_from_file() function
+btn_enter = tk.Button(
+    master=window, 
+    text='Enter Values From a File',
+    command=enter_from_file
+)
+
+# Create a button to call the save_to_file() function
+btn_save = tk.Button(
+    master=window,
+    text="Export Passed Comparisons",
+    command=save_to_file
+)
+
 #lay the frames out using .grid()
 frm_title.grid(row=0, column=0)
 frm_first.grid(row=1, column=0, padx=50)
 frm_second.grid(row=2, column=0, pady=20)
 lbl_result.grid(row=3, column=0, padx=50, pady=20, sticky="nsew")
-btn_compare.grid(row=4, column=0, sticky="nsew")
+btn_compare.grid(row=4, column=0, sticky="nw")
 frm_memory.grid(row=0, column=1)
+btn_enter.grid(row=5, column=0, sticky="sw")
+btn_save.grid(row=4, column=0, sticky="ne")
+btn_help.grid(row=5, column=0, sticky="se")
 
 #run the program
 window.mainloop()
